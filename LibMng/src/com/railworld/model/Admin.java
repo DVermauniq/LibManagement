559c7bfac51c1790fe8fecb086ab474f420f5c0e
deepakverma <deepakvermaawesome@gmail.com>
2023-11-28 12:24:55 +0530

    added model classes

--- LibMng/src/com/railworld/model/Admin.java
+++ LibMng/src/com/railworld/model/Admin.java
@@ -1,5 +1,41 @@
 package com.railworld.model;
 
 public class Admin {
+	private int aid;
+	private String Name;
+	private String password;
+	public Admin() {
+		// TODO Auto-generated constructor stub
+	}
+	public Admin(int aid, String name, String password) {
+		super();
+		this.aid = aid;
+		Name = name;
+		this.password = password;
+	}
+	public int getAid() {
+		return aid;
+	}
+	public void setAid(int aid) {
+		this.aid = aid;
+	}
+	public String getName() {
+		return Name;
+	}
+	public void setName(String name) {
+		Name = name;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	@Override
+	public String toString() {
+		return "Admin [aid=" + aid + ", Name=" + Name + ", password=" + password + "]";
+	}
+	
+	
 
 }
--- LibMng/src/com/railworld/model/Books.java
+++ LibMng/src/com/railworld/model/Books.java
@@ -2,4 +2,48 @@ package com.railworld.model;
 
 public class Books {
 
+	private int bId;
+	private String name;
+	private String author;
+	private int copies;
+	@Override
+	public String toString() {
+		return "Books [bId=" + bId + ", name=" + name + ", author=" + author + ", copies=" + copies + "]";
+	}
+	public int getbId() {
+		return bId;
+	}
+	public void setbId(int bId) {
+		this.bId = bId;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getAuthor() {
+		return author;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	public int getCopies() {
+		return copies;
+	}
+	public void setCopies(int copies) {
+		this.copies = copies;
+	}
+	public Books() {
+		
+		// TODO Auto-generated constructor stub
+	}
+	public Books(int bId, String name, String author, int copies) {
+		super();
+		this.bId = bId;
+		this.name = name;
+		this.author = author;
+		this.copies = copies;
+	}
+	
 }
