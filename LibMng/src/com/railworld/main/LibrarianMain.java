f7d1481b5f6d16ebba696178812e088982e9c03e
Yugansh-Shrivastava <yugansh0.4shrivastava@gmail.com>
2023-11-29 11:53:11 +0530

    main

---
+++ LibMng/src/com/railworld/main/LibrarianMain.java
@@ -0,0 +1,11 @@
+package com.railworld.main;
+
+import java.util.List;
+import java.util.Scanner;
+
+import com.railworld.*;
+
+public class LibrarianMain {
+	public static void main(String[] args) {
+	}
+}
